3733e3414dcf29e2e69df3cc12b21b21fee57934
manijang2 <manijang2@naver.com>
2024-10-25 11:23:00 +0900

    init

---
+++ src/main/java/me/smtalk/api/SmtalkApiApplication.java
@@ -0,0 +1,13 @@
+package me.smtalk.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtalkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmtalkApiApplication.class, args);
+    }
+
+}
